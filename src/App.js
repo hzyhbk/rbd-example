8ca65b554edf1f1dd49db05eac28e26f8a2bc3d3
huangz <zhangyuhang@souche.com>
2020-06-30 14:15:30 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import { hot } from "react-hot-loader/root";
+import Dnd from "./Dnd";
+
+function App() {
+  return (
+    <div className="App">
+      <div>hmrss</div>
+      <Dnd />
+    </div>
+  );
+}
+
+export default hot(App);
